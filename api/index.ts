0e5a9a3b440df990316d83e7ea0adfd13d2e260f
SinethembaDlova <dSinethemba@gmail.com>
2017-11-08 12:50:28 +0200

    re-arranged my folder structure

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import {Server} from './server';
-import {HomeRoute} from './routes/homeRoute';
-
-
-var server = new Server();
-var homeRoute = new HomeRoute
-
-server.app.get('/', homeRoute.landHome);
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,32 @@
+import {Server} from './api/server';
+import {HomeRoute} from './api/routes/homeRoute';
+
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import {relationshipSchema} from "./entity/relationshipSchema";
 
 createConnection().then(async connection => {
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Ayabonga";
-    user.lastName = "Zondile";
-    user.age = 29;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+    console.log("Inserting a new relationshipSchema into the database...");
+    const relationship = new relationshipSchema();
+    relationship.parentEntityId = 0;
+    relationship.parentEntityName = "";
+    relationship.relationshipType = "";
+    relationship.entityId = 0;
+    relationship.entityName = "";
+    await connection.manager.save(relationship);
+    console.log("Saved a new relationshipSchema with id: " + relationship.id);
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+    console.log("Loading relationshipSchemas from the database...");
+    const relationshipSchemas = await connection.manager.find(relationshipSchema);
+    console.log("Loaded relationship: ", relationship);
 
     console.log("Here you can setup and run express/koa/any other framework.");
 
 }).catch(error => console.log(error));
+
+
+var server = new Server();
+var homeRoute = new HomeRoute
+
+server.app.get('/', homeRoute.landHome);
